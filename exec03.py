ef23fb463b9b71588cc3b6f70c5f545d9b86003a
Natanael Alves Palmeira <Natanaelalves.contato@gmail.com>
2026-06-19 15:18:53 -0300

    execccc

--- exec03.py
+++ exec03.py
@@ -1,4 +1,4 @@
 p1 =  int(input('Primeiro Número: '))
 p2 =  int(input('Segundo Número: '))
 soma = p1 + p2
-print('A soma entre', p1, 'e', p2, 'é igual a', soma)
+print('A soma entre {} e {} é igual a {}'.format(p1, p2, soma))
