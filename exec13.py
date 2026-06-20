b5e773e0e5bf131d8caeaefb3b94a435d490242e
Natanael Alves Palmeira <Natanaelalves.contato@gmail.com>
2026-06-20 16:11:00 -0300

    exec13.py

--- exec13.py
+++ exec13.py
@@ -1,3 +1,3 @@
 n1 = float(input('Digite o Preço Inicial do Produto: R$'))
 n2 = n1 * 0.95
-print('O Novo Valor do Produto com 5% de Desconto é {}'.format(n2))
+print('O Novo Valor do Produto com 5% de Desconto é {:.2f}'.format(n2))
