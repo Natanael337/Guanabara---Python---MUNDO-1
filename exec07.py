60e7f5abcd6b269fb36ce5787f104bc9bdfaeadd
Natanael Alves Palmeira <Natanaelalves.contato@gmail.com>
2026-06-20 15:38:56 -0300

    exec07.py

---
+++ exec07.py
@@ -0,0 +1,5 @@
+n1 = int(input('Digite um Número: '))
+n2 = n1 + n1
+n3 = n1 + n1 + n1
+n0 = n1 ** (1/2)
+print('O Dobro desse Número é {}, seu Triplo é {} e sua Raiz Quadrada é {}!'.format(n2, n3, n0))
