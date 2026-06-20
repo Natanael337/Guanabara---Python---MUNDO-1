771ba54eaee90f25acaf6d2946e8b313aa60c509
Natanael Alves Palmeira <Natanaelalves.contato@gmail.com>
2026-06-20 14:04:10 -0300

    exec5.py

--- exec05.py
+++ exec05.py
@@ -1,1 +1,10 @@
-
+n1 = int(input('Digite um Valor: '))
+n2 = int(input('Digite outro Valor: '))
+s = n1 + n2
+sb = n1 - n2
+d = n1 / n2
+m = n1 * n2
+di = n1 // n2
+p = n1 ** n2
+print('A Soma é {}, a Substração é {}, a Multiplicação é {} e a Divisão {:.3f}'.format(s, sb, m, d))
+print('A Divisão Inteira é {} e a Potência é {}'.format(di, p))
