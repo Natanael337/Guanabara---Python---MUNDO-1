882fef479d17ebc9d0bfc97832971f142ad8bc6f
Natanael Alves Palmeira <Natanaelalves.contato@gmail.com>
2026-06-27 14:57:09 -0300

    exec24.py

--- exec23.py
+++ exec23.py
@@ -1,3 +1,3 @@
 cidade = input('Digite o nome da cidade: ')
 cidade = cidade.upper()
-print(cidade.find('SANTO') + 1)
+print('O Nome da sua Cidade possuí SANTO? {}'.format('SANTO' in cidade))
---
+++ exec24.py
@@ -0,0 +1,3 @@
+nome = input('Digite seu nome: ')
+nome = nome.upper()
+print('Seu nome tem Silva? {}'.format('SILVA' in nome))
