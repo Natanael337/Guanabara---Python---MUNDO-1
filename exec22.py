10274b90a63f15df57a6f1125a72fe284c950b6b
Natanael Alves Palmeira <Natanaelalves.contato@gmail.com>
2026-06-27 14:49:11 -0300

    exec22.py

---
+++ exec22.py
@@ -0,0 +1,6 @@
+import math
+n = (input('Digite um Número Inteiro entre 0 e 9999: '))
+print('Unidade: {}'.format(n[3]))
+print('Dezena: {}'.format(n[2]))
+print('Centena: {}'.format(n[1]))
+print('Milhar: {}'.format(n[0]))
